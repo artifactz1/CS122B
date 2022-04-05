36b88a6aa91d92fee7cb6a2fbc276e2c97c340ad
Player Hi <punsalang.art@gmail.com>
2022-04-04 19:03:13 -0700

    hw2 : reverse part finished

--- CS122B-HW2-Basic-Service-Starter-main/CS122B-HW2-Basic-Service-Starter-main/src/main/java/com/github/klefstad_teaching/cs122b/basic/rest/BasicController.java
+++ CS122B-HW2-Basic-Service-Starter-main/CS122B-HW2-Basic-Service-Starter-main/src/main/java/com/github/klefstad_teaching/cs122b/basic/rest/BasicController.java
@@ -2,14 +2,20 @@ package com.github.klefstad_teaching.cs122b.basic.rest;
 
 import com.github.klefstad_teaching.cs122b.basic.config.BasicServiceConfig;
 import com.github.klefstad_teaching.cs122b.basic.response.HelloResponse;
+import com.github.klefstad_teaching.cs122b.basic.response.ReverseResponse;
 import com.github.klefstad_teaching.cs122b.basic.util.Validate;
+import com.github.klefstad_teaching.cs122b.core.error.ResultError;
 import com.github.klefstad_teaching.cs122b.core.result.BasicResults;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.regex.*;
 
 @RestController
 public class BasicController {
@@ -33,4 +39,28 @@ public class BasicController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(body);
     }
+
+    @GetMapping("/reverse/{message}")
+    public ResponseEntity<ReverseResponse> reverse(@PathVariable String message) {
+        String rev = new StringBuilder(message).reverse().toString();
+        ReverseResponse good = new ReverseResponse()
+                .setReversed(rev)
+                .setResult(BasicResults.STRING_SUCCESSFULLY_REVERSED);
+
+        if (message.isEmpty() == true || message.equals(" ")) {
+            throw new ResultError(BasicResults.STRING_IS_EMPTY);
+        }
+
+        String regex = "^[a-zA-Z0-9_\\s]+$";
+
+        if (message.matches(regex) == false) {
+            throw new ResultError(BasicResults.STRING_CONTAINS_INVALID_CHARACTERS);
+
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(good);
+
+    }
+
 }
